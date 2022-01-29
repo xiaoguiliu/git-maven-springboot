236a300a26e156d2fdff10c5b48a8d29428793ce
brucexiaogui <Brucedalong@163.com>
2022-01-29 17:34:52 +0800

    第一次提交项目到本地Git仓库

---
+++ src/test/java/com/bruce/git/GitMavenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruce.git;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitMavenSpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
